7b5d827a1e8fa3c90b1df414e61552afc91538dc
Denis <dovzhanyndenis03@gmail.com>
2025-04-30 18:21:49 -0400

    feat: added tests for login route

--- __tests__/routesTest.ts
+++ __tests__/routesTest.ts
@@ -1,8 +1,9 @@
 import { Db,Collection } from "mongodb"
 import { closeConnection, connectDb, getDb } from "../src/mongodb"
-import { deleteUser, insertUser } from "../src/db/user"
-import request from 'supertest'
+import { deleteUser, findByUsername, insertUser } from "../src/db/user"
+import request, { Response } from 'supertest'
 import app from "../src/app"
+import { User } from "../src/models/user"
 
 beforeAll(async () => {
     await connectDb()
@@ -34,6 +35,26 @@ describe('POST /auth/register', () => {
         expect(emptyPasswordResponse.body.error).toMatch('Username or Password missing')
     })
 }) 
+
+describe('POST /auth/login', () => {
+    
+    let response: Response
+
+    it('should allow users to log in', async () => {
+        response = await request(app).post('/auth/login').send({username: 'test', password: 'test'})
+        expect(response.statusCode).toBe(200)
+        expect(response.headers['set-cookie'][0]).toContain('sessionToken')
+        expect(response.body).toHaveProperty('id')
+
+        
+    })
+    it('should update the users cookie in mongodb', async () => {
+        const user: User = await findByUsername('test') as User
+        const lastLetterOfCookieIndex = response.headers['set-cookie'][0].indexOf(';')
+        // substring(12) because response.headers['set-cookie'][0] will look something like 'sessionToken=blahblahblah'
+        expect(user.authorization.sessionToken).toMatch(response.headers['set-cookie'][0].substring(13, lastLetterOfCookieIndex))
+    })
+})
 
 afterAll(async () => {
     await deleteUser('test')
